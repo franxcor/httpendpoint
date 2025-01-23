d65eb0e4e1a7786e22ac18947f64ae3c16fc1c03
franxcor <129801200+franxcor@users.noreply.github.com>
2025-01-23 11:02:34 -0500

    fetchall & execute

--- webserverPractice.py
+++ webserverPractice.py
@@ -5,18 +5,39 @@ serverPort = 8080
 
 import psycopg2
 
-connection = psycopg2.connect (
-    dbname="subscribers",
-    user="postgres",
-    password="Temporary123",
-    host="localhost"
-)
+
+
 
 class BasicServer(BaseHTTPRequestHandler):
+    def connect(self) :
+        conn = None
+        try:  
+            print("connecting")
+            conn = psycopg2.connect(
+                dbname="postgres", #this is database name not table name
+                user="postgres",
+                password="Temporary123",
+                host="localhost"
+            )
+
+            #cursor for statements
+            cur = conn.cursor()
+
+            cur.execute('SELECT imsi FROM subscribers;') #execute to do the actual SQL commands
+            fetchedData = cur.fetchall() #fetchall to fetch the data from the SQL command
+            print(fetchedData)
+        except (psycopg2.DatabaseError) as error :
+            print(error) #in case there is an error in the database
+        finally:
+            if conn is not None:
+                conn.close() #closing the database connection
+                print('Database connection closed')
+
     def do_GET(self):
         self.send_response(200) #status code for a successful request
         self.send_header("Content-type", "text/html") #adds header data to http response (name of header and value)
         self.end_headers()
+        self.connect()
 
         #just content on the page
         self.wfile.write(bytes("<html><head></head>", "utf-8"))
@@ -24,6 +45,8 @@ class BasicServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p> a web server </p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
+
+
 if __name__ == "__main__" :
     webServer = HTTPServer((hostName, serverPort), BasicServer) #starting the actual server
     print("Server url http://%s:%s" % (hostName, serverPort)) #creating link
